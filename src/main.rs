009872d849a16347daaddc4333b70e4da3f0402c
Sam Aardema <samaardema@sijthoffmedia.nl>
2023-03-02 23:54:42 +0100

    Start from level 1

--- src/main.rs
+++ src/main.rs
@@ -53,7 +53,7 @@ impl Default for PlayerProgress {
         PlayerProgress {
             score: 0.,
             balls_remaining: 3,
-            level: 4,
+            level: 1,
         }
     }
 }
